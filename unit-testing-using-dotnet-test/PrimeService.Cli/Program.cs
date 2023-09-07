c028676e13725875acf9d6b1d635419a3b269ae7
Tandurin <dennisvlaanderen@gmail.com>
2023-09-07 12:18:58 +0200

    #14-controle-geldigheid-parameters

--- unit-testing-using-dotnet-test/PrimeService.Cli/Program.cs
+++ unit-testing-using-dotnet-test/PrimeService.Cli/Program.cs
@@ -14,8 +14,14 @@ class Program
             return;
         }
 
-        Int32.TryParse(args[0], out var getal);
+        var conversionSucceeded = Int32.TryParse(args[0], out var getal);
 
+        if (!conversionSucceeded)
+        {
+            Console.WriteLine("Opgegeven parameter is geen geldig getal.");
+            return;
+        }
+        
         var isPriemgetal = PrimeService.IsPrime(getal) ? "Ja" : "Nee";
         
         Console.WriteLine($"Is {getal} een priemgetal? -> {isPriemgetal}");
