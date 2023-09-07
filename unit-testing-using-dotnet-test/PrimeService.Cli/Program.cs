14c9f0127d04dff5bf0158569d6d153a2119217e
Tandurin <dennisvlaanderen@gmail.com>
2023-09-07 11:53:56 +0200

    #14-cli-project-toegevoegd

---
+++ unit-testing-using-dotnet-test/PrimeService.Cli/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace PrimeService.Cli;
+
+class Program
+{
+    private static readonly PrimeService PrimeService = new BardPrimeService();
+    
+    public static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Geen getal opgegeven als argument. Geef een integer getal op om te checken.");
+            return;
+        }
+
+        Int32.TryParse(args[0], out var getal);
+
+        var isPriemgetal = PrimeService.IsPrime(getal) ? "Ja" : "Nee";
+        
+        Console.WriteLine($"Is {getal} een priemgetal? -> {isPriemgetal}");
+    }
+}
